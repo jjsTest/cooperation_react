7ce64f5121588e18e26c1cb9ef6997f79b8e11fa
jjsTest <julee00211@gmail.com>
2021-11-06 22:07:36 +0900

    Update Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,6 +6,24 @@ import AddBoardModal from '../modals/AddBoardModal.js';
 import axios from 'axios';
 //import BoardRoom from './BoardRoom.js';
 import Header from './Header';
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableBody,
+  CTableDataCell,
+  CDataTable,
+  CRow
+} from '@coreui/react';
+//import { DocsLink } from 'src/reusable'
+
+//import usersData from '../../users/UsersData'
 
 function Home() {
 
@@ -13,6 +31,7 @@ function Home() {
   const [modalState, setModalState] = useState(false); // 모달 open 여부
   const [boardList, setBoardList] = useState([]);
   const [boardId, setBoardId] = useState("init");
+  const fields = ['name','num', 'total_num', 'create_id'];
   //const [nameValue, setNameValue] = useState(''); // 이름
   //const [countValue, setCountValue] = useState(0); // 사람 수 
 
@@ -85,34 +104,60 @@ function Home() {
 
     return (
       <div>
-        <Header />
-        
-          {/* 보드 추가 모달창 */}
-          <button onClick={addBoardModalOpen}>AddBoard</button>  
-          <Modal isOpen ={modalState}>
-            <AddBoardModal addBoardModalClose={addBoardModalClose} boardId={boardId}/>
-          </Modal>
-          <hr></hr>
-          <div className="container">
-            {boardList.map((board) => (
-              <div key={boardList.id} className="item">
-                {board.create_id=="j" ? <button onClick={() => editBoard(board.id)}>edit</button>: null}
-                {board.create_id=="j" ? <button onClick={() => deleteBoard(board.id)}>delete</button>: null}
-                {/* if({board.create_id} === "j"){ <button>edit</button>} */}
-                <p>name: {board.name}</p>
-                <p>{board.num} / {board.total_num}</p>
-                <Link to={`/BoardRoom/${board.id}`}><button>enter</button></Link>
-                {/* ` : 템플릿 문자열 이때 변수는 ${}안에 넣어서 보내줘야해서 위처럼 사용함. */}
-                {/* <Link to ={{
-                  pathname:"/boardRoom",
-                  search:`?board_id=${board.id}`
-                }}> <button>enter</button> </Link> */}
+        <Header />   
+        {/* 보드 추가 모달창 */}
+        <button onClick={addBoardModalOpen}>AddBoard</button>  
+        <Modal isOpen ={modalState}>
+          <AddBoardModal addBoardModalClose={addBoardModalClose} boardId={boardId}/>
+        </Modal>
+        <hr></hr>
+        {/* <CRow>*/}
+          {/* <CCol xs="12" lg="6"> */}
+         {/* <CCol xs="12" lg="20">
+            <CCard>
+              <CCardHeader>
+                Striped Table
+              </CCardHeader>  */}
+              <CTable striped>
+                <CTableHead>
+                  <CTableRow>
+                    <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">board</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">create_id</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">현인원/최대인원</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                  <CTableRow>
+                    {boardList.map((board) => (
+                      <div key={boardList.id}>
+                        <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                        <CTableDataCell scope="row">{board.name}</CTableDataCell>
+                        <CTableDataCell scope="row">{board.create_id}</CTableDataCell>
+                        <CTableDataCell scope="row">{board.num} / {board.total_num}</CTableDataCell>
+                        <CTableDataCell scope="row">{board.create_id=="j" ? <button onClick={() => editBoard(board.id)}>edit</button>: null}
+                        {board.create_id=="j" ? <button onClick={() => deleteBoard(board.id)}>delete</button>: null}
+                        {/* if({board.create_id} === "j"){ <button>edit</button>} */}
+                        <Link to={`/BoardRoom/${board.id}`}><button>enter</button></Link></CTableDataCell>
+                        {/* ` : 템플릿 문자열 이때 변수는 ${}안에 넣어서 보내줘야해서 위처럼 사용함. */}
+                        {/* <Link to ={{
+                          pathname:"/boardRoom",
+                          search:`?board_id=${board.id}`
+                        }}> <button>enter</button> </Link> */}
 
-              </div>
-            ))}
-          </div>
+                      </div>
+                    ))}
+                  </CTableRow>
+                </CTableBody>
+              </CTable>
+            {/* </CCard>
+          </CCol>
+        </CRow>  */}
       </div>
     );
   }
 
   export default Home;
+
+  //https://coreui.io/react/docs/4.0/components/table/
